bf6d68990656a1d74eea5236090e0ed7674b1abd
Devashishsaurabh <onlyvishwasindia@gmail.com>
2022-05-19 20:44:10 +0530

    Mobile-os

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Heading from "./components/Heading.jsx";
+import List from './components/List.jsx';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const mobileOS = ["Android", "Blackberry", "I Phone", "Windows Phone"];
+  const mobileMan = ["Samsung", "HTC", "Micromax", "Apple"];
+return (
+   <div className="App">
+   <Heading head={"Mobile Operating System"}/>
+   <List data = {mobileOS}/>
+   <Heading head={"Mobile Manufacturers"} />
+   <List data = {mobileMan}/>
+   </div>
   );
 }
 
